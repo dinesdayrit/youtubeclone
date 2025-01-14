a23a639795e35f175e78f4a98798897c7122ddaf
dinesdayrit <dayritdines22@gmail.com>
2025-01-14 19:12:08 +0800

    add upload, bell, user icon on header

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,18 +1,29 @@
-import { Menu } from "lucide-react";
+import { Bell, Menu, Upload, User } from "lucide-react";
 import logo from "../assets/Logo.png";
+import { Button } from "../components/Button";
 export default function PageHeader() {
   return (
-    <div className="flex gap-10 lg:gap-20 justify-between">
+    <div className="flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4">
       <div className="flex gap-4 items-center flex-shrink-0">
-        <button>
+        <Button variant="ghost">
           <Menu />
-        </button>
+        </Button>
         <a href="/">
           <img src={logo} className="h-6" />
         </a>
       </div>
       <div></div>
-      <div></div>
+      <div className="flex flex-shrink-0 md:gap-2">
+        <Button size="icon" variant="ghost">
+          <Upload />
+        </Button>
+        <Button size="icon" variant="ghost">
+          <Bell />
+        </Button>
+        <Button size="icon" variant="ghost">
+          <User />
+        </Button>
+      </div>
     </div>
   );
 }
