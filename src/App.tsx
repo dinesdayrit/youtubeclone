5d0a53945d6cd8dcfbabe27ec2ac74ac70390a31
dinesdayrit <dayritdines22@gmail.com>
2025-01-23 19:47:07 +0800

    changing button bgcolor when selected on category

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
+import { useState } from "react";
 import CategoryPills from "./components/CategorytPills";
 import { categories } from "./data/home";
 import PageHeader from "./layouts/PageHeader";
 
 export default function App() {
+  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
   return (
     <div className="max-h-screen flex flex-col">
       <PageHeader />
       <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
         <div>Sidebar</div>
         <div className="sticky top-0 bg-white z-10 pb-4">
-          <CategoryPills categories={categories} />
+          <CategoryPills
+            categories={categories}
+            selectedCategory={selectedCategory}
+            onSelect={setSelectedCategory}
+          />
         </div>
       </div>
     </div>
--- src/components/CategorytPills.tsx
+++ src/components/CategorytPills.tsx
@@ -1,20 +1,27 @@
 import { Button } from "./Button";
 type CategoryPillProps = {
   categories: string[];
+  selectedCategory: string;
+  onSelect: (category: string) => void;
 };
-export default function CategorytPills({ categories }: CategoryPillProps) {
+export default function CategorytPills({
+  categories,
+  selectedCategory,
+  onSelect,
+}: CategoryPillProps) {
   return (
     <div className="overflow-x-hidden relative">
       <div className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]">
-        <Button
-          variant="dark"
-          className="py-1 px-3 rounded-lg whitespace-nowrap"
-        >
-          All
-        </Button>
-        <Button className="py-1 px-3 rounded-lg whitespace-nowrap">
-          Javascript
-        </Button>
+        {categories.map((category) => (
+          <Button
+            key={category}
+            onClick={() => onSelect(category)}
+            variant={selectedCategory === category ? "dark" : "default"}
+            className="py-1 px-3 rounded-lg whitespace-nowrap"
+          >
+            {category}
+          </Button>
+        ))}
       </div>
     </div>
   );
