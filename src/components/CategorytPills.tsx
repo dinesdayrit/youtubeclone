33ab7afe72e8d7f66551ccd8ffbdf59c4aa80c5e
dinesdayrit <dayritdines22@gmail.com>
2025-01-23 19:33:21 +0800

    update categorypills

--- src/components/CategorytPills.tsx
+++ src/components/CategorytPills.tsx
@@ -3,8 +3,17 @@ import { Button } from "./Button";
 export default function CategorytPills() {
   return (
     <div className="overflow-x-hidden relative">
-      <div className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]"></div>
-      <Button className="py-1 px-3 rounded-lg whitespace-nowrap">All</Button>
+      <div className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]">
+        <Button
+          variant="dark"
+          className="py-1 px-3 rounded-lg whitespace-nowrap"
+        >
+          All
+        </Button>
+        <Button className="py-1 px-3 rounded-lg whitespace-nowrap">
+          Javascript
+        </Button>
+      </div>
     </div>
   );
 }
