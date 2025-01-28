3a1c519f71bcddc0caa2d85f745f05a0f51c7045
dinesdayrit <dayritdines22@gmail.com>
2025-01-28 15:15:59 +0800

    add home Icon on sidebar

--- src/layouts/SideBar.tsx
+++ src/layouts/SideBar.tsx
@@ -1,3 +1,27 @@
+import { Home } from "lucide-react";
+import { ElementType } from "react";
+
 export default function SideBar() {
-  return <div>SideBar</div>;
+  return (
+    <aside
+      className={`sticky top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col ml-1 `}
+    >
+      <SmallSidebarItem Icon={Home} title="Home" url="/" />
+    </aside>
+  );
+}
+
+type SmallSidebarItemProps = {
+  Icon: ElementType;
+  title: string;
+  url: string;
+};
+
+function SmallSidebarItem({ Icon, title, url }: SmallSidebarItemProps) {
+  return (
+    <a href={url} className="">
+      <Icon className="w-6 h-6" />
+      <div className="text-sm">{title}</div>
+    </a>
+  );
 }
